47a717635f97b9094965768b539394fd64129a99
Alexandru Tiniuc <a.tiniuc@cascoda.com>
2021-02-24 14:55:36 +0000

    eliminate memory allocation in separate responses

--- messaging/coap/oc_coap.h
+++ messaging/coap/oc_coap.h
@@ -20,6 +20,7 @@
 #include "separate.h"
 #include "util/oc_list.h"
 #include "oc_ri.h"
+#include "oc_blockwise.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -29,11 +30,7 @@ struct oc_separate_response_s
 {
   OC_LIST_STRUCT(requests);
   int active;
-#ifdef OC_DYNAMIC_ALLOCATION
-  uint8_t *buffer;
-#else  /* OC_DYNAMIC_ALLOCATION */
-  uint8_t buffer[OC_MAX_APP_DATA_SIZE];
-#endif /* !OC_DYNAMIC_ALLOCATION */
+  oc_blockwise_state_t *response_state;
 };
 
 struct oc_response_buffer_s

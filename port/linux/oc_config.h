9715dd87bde32a51ef294292f4e3739fc1312be1
Alexandru Tiniuc <a.tiniuc@cascoda.com>
2021-03-31 13:07:50 +0100

    import embedded oc_config.h file

--- port/linux/oc_config.h
+++ port/linux/oc_config.h
@@ -1,120 +1,91 @@
+/*
+ // Copyright (c) 2016 Intel Corporation
+// Modifications copyright (c) 2020, Cascoda Ltd.
+ //
+ // Licensed under the Apache License, Version 2.0 (the "License");
+ // you may not use this file except in compliance with the License.
+ // You may obtain a copy of the License at
+ //
+ //      http://www.apache.org/licenses/LICENSE-2.0
+ //
+ // Unless required by applicable law or agreed to in writing, software
+ // distributed under the License is distributed on an "AS IS" BASIS,
+ // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ // See the License for the specific language governing permissions and
+ // limitations under the License.
+ */
+
 #ifndef OC_CONFIG_H
 #define OC_CONFIG_H
 
 /* Time resolution */
 #include <stdint.h>
-#include <time.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/* enable flash storage API */
+#define OC_STORAGE
+
 typedef uint64_t oc_clock_time_t;
-#define OC_CLOCK_CONF_TICKS_PER_SECOND CLOCKS_PER_SEC
-//#define OC_SPEC_VER_OIC
-/* Security Layer */
-/* Max inactivity timeout before tearing down DTLS connection */
-#define OC_DTLS_INACTIVITY_TIMEOUT (600)
+/* 1 clock tick = 1ms */
+#define OC_CLOCK_CONF_TICKS_PER_SECOND (1000)
 
-/* Maximum wait time for select function */
-#define SELECT_TIMEOUT_SEC (1)
-
-/* Add support for passing network up/down events to the app */
-#define OC_NETWORK_MONITOR
-/* Add support for passing TCP/TLS/DTLS session connection events to the app */
-#define OC_SESSION_EVENTS
-/* Add request history for deduplicate UDP/DTLS messages */
-#define OC_REQUEST_HISTORY
-
-/* Add support for software update */
-//#define OC_SOFTWARE_UPDATE or run "make" with SWUPDATE=1
-/* Add support for the oic.if.create interface in Collections */
-//#define OC_COLLECTIONS_IF_CREATE or run "make" with CREATE=1
-/* Add support for the maintenance resource */
-//#define OC_MNT or run "make" with MNT=1
-/* Add batch interface support to /oic/res */
-#define OC_RES_BATCH_SUPPORT
-/* Add support for dns lookup to the endpoint */
-#define OC_DNS_LOOKUP
-#define OC_DNS_CACHE
-//#define OC_DNS_LOOKUP_IPV6
-
-/* If we selected support for dynamic memory allocation */
-#ifdef OC_DYNAMIC_ALLOCATION
-#define OC_COLLECTIONS
-#define OC_BLOCK_WISE
-
-#else /* OC_DYNAMIC_ALLOCATION */
-/* List of constraints below for a build that does not employ dynamic
-   memory allocation
-*/
-/* Memory pool sizes */
-#define OC_BYTES_POOL_SIZE (1800)
-#define OC_INTS_POOL_SIZE (100)
-#define OC_DOUBLES_POOL_SIZE (4)
+#define OC_BYTES_POOL_SIZE (16 * 1024)
+#define OC_INTS_POOL_SIZE (16)
+#define OC_DOUBLES_POOL_SIZE (16)
 
 /* Server-side parameters */
 /* Maximum number of server resources */
-#define OC_MAX_APP_RESOURCES (4)
-
-#define OC_MAX_NUM_COLLECTIONS (1)
+#define OC_MAX_APP_RESOURCES (1)
 
 /* Common paramters */
-/* Prescriptive lower layers MTU size, enable block-wise transfers */
-#define OC_BLOCK_WISE_SET_MTU (700)
+/* Maximum size of request/response PDUs */
+#ifndef OC_DYNAMIC_ALLOCATION
+#define OC_MAX_APP_DATA_SIZE (8192)
+#endif // !OC_DYNAMIC_ALLOCATION
 
-/* Maximum size of request/response payloads */
-#define OC_MAX_APP_DATA_SIZE (2048)
+#define OC_INOUT_BUFFER_POOL (2)
+#define OC_INOUT_BUFFER_SIZE (1232)
+
+#define OC_APP_DATA_BUFFER_POOL (1)
+#define OC_APP_DATA_BUFFER_SIZE (8192)
+
+// The maximum size of a response to an OBSERVE request, in bytes.
+#define OC_MAX_OBSERVE_SIZE 512
+
+// Deduplicate CoAP messages
+#define OC_REQUEST_HISTORY
+
+//#define OC_APP_DATA_STORAGE_BUFFER
+
+#define OC_BLOCK_WISE
+#define OC_BLOCK_WISE_SET_MTU (1200)
 
 /* Maximum number of concurrent requests */
-#define OC_MAX_NUM_CONCURRENT_REQUESTS (3)
+#define OC_MAX_NUM_CONCURRENT_REQUESTS (2)
 
 /* Maximum number of nodes in a payload tree structure */
-#define OC_MAX_NUM_REP_OBJECTS (150)
+#define OC_MAX_NUM_REP_OBJECTS (70)
 
 /* Number of devices on the OCF platform */
-#define OC_MAX_NUM_DEVICES (2)
+#define OC_MAX_NUM_DEVICES (1)
 
-/* Maximum number of endpoints */
-#define OC_MAX_NUM_ENDPOINTS (20)
+#define OC_MAX_NUM_ENDPOINTS (2)
 
 /* Security layer */
 /* Maximum number of authorized clients */
-#define OC_MAX_NUM_SUBJECTS (2)
+#define OC_MAX_NUM_SUBJECTS (1)
 
-/* Maximum number of concurrent (D)TLS sessions */
-#define OC_MAX_TLS_PEERS (1)
+/* Maximum number of concurrent DTLS sessions */
+#define OC_MAX_DTLS_PEERS (1)
 
-/* Maximum number of peer for TCP channel */
-#define OC_MAX_TCP_PEERS (2)
-
-/* Maximum number of interfaces for IP adapter */
-#define OC_MAX_IP_INTERFACES (2)
-
-/* Maximum number of callbacks for Network interface event monitoring */
-#define OC_MAX_NETWORK_INTERFACE_CBS (2)
-
-/* Maximum number of callbacks for connection of session */
-#define OC_MAX_SESSION_EVENT_CBS (2)
-
-#endif /* !OC_DYNAMIC_ALLOCATION */
-
-/* library features that require persistent storage */
-#ifdef OC_SECURITY
-#define OC_STORAGE
-#endif
-#ifdef OC_IDD_API
-#define OC_STORAGE
-#endif
-#ifdef OC_CLOUD
-#define OC_STORAGE
-#endif
-#ifdef OC_SOFTWARE_UPDATE
-#define OC_STORAGE
-#endif
+/* Max inactivity timeout before tearing down DTLS connection */
+#define OC_DTLS_INACTIVITY_TIMEOUT (600)
 
 #ifdef __cplusplus
-}
+} // extern "C"
 #endif
 
 #endif /* OC_CONFIG_H */
